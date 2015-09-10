a4b3672c26e4082e95b43386e39f4b5d0080d81e
Brandon Trumble <brandon.trumble@gmail.com>
2015-09-10 14:04:15 -0400

    Chat

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
 var appPort = process.env.PORT || 5000;
@@ -12,6 +13,18 @@ app.get('/', function(request, response){
 //Route for css, js
 app.get('/public/:name', function(request, response){
 	response.sendFile(__dirname + '/public/' + request.params.name)
+});
+
+//Socket handling
+io.on('connection', function(socket){
+	//User connect messages
+	socket.on('statusMessage', function(message){
+		io.emit('statusMessage', message);
+	});
+	//Chat messages
+	socket.on('chatMessage', function(message){
+		io.emit('chatMessage', message);
+	});
 });
 
 //Start node server
