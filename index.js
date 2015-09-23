804b7bb93e674a6ea24dc8952793a58fe09be132
Brandon Trumble <brandon.trumble@gmail.com>
2015-09-23 13:53:16 -0400

    Hello world!

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http');
 
-app.set('port', (process.env.PORT || 5000));
-//var appPort = process.env.PORT || 5000;
+var appPort = process.env.PORT || 5000;
 
-//Route for html
-app.get('/', function(request, response){
-	response.sendFile(__dirname + '/public/index.html');
+var server = http.createServer(function (request, response){
+	response.writeHead(200, {"Content-Type": "text-plain"});
+	response.end("Hello world!");
 });
 
-//Route for css, js
-app.get('/public/:name', function(request, response){
-	response.sendFile(__dirname + '/public/' + request.params.name)
-});
-
-//Socket handling
-io.on('connection', function(socket){
-	//User connect messages
-	socket.on('statusMessage', function(message){
-		io.emit('statusMessage', message);
-	});
-	//Chat messages
-	socket.on('chatMessage', function(message){
-		io.emit('chatMessage', message);
-	});
-});
-
-//Start node server
-http.listen(app.get('port'), function(){
-	console.log('Server running at localhost:' + app.get('port'));	
+server.listen(appPort, function(){
+	console.log('Server Listening @localhost:' + appPort);
 });
